41b5f22289b4b82810d7541e6d46f0e24ded1480
Ryan Murphy <TheDecoder007@gmail.com>
2022-07-30 16:42:02 -0400

    changed code to match snapshot, errors on localhost:8080

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require("path");
 
-module.exports = {
+const config = {
   entry: {
     app: "./assets/js/script.js",
     events: "./assets/js/events.js",
@@ -12,13 +12,13 @@ module.exports = {
   },
 
   output: {
-    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: "file-loader",
@@ -44,9 +44,25 @@ module.exports = {
       $: "jquery",
       jQuery: "jquery",
     }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-    }),
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    // }),
+     // new WebpackPwaManifest({
+    //   name: "Food Event",
+    //   short_name: "Foodies",
+    //   description: "An app that allows you to view upcoming food events.",
+    //   background_color: "#01579b",
+    //   theme_color: "#ffffff",
+    //   fingerprints: false,
+    //   inject: false,
+    //   icons: [{
+    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join("assets", "icons")
+    //   }]
+    // })
   ],
   mode: "development",
 };
+
+module.exports = config;
